7909a167dbe284eabe42494ac526f69697b175a0
wxfeng <1521255410@qq.com>
2020-06-30 22:14:53 +0800

    conflict

--- src/main/java/com/xfeng/demo/DemoApplication.java
+++ src/main/java/com/xfeng/demo/DemoApplication.java
@@ -16,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class DemoApplication {
 
+    //conflict test
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
